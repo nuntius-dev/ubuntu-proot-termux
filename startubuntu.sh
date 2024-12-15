288ba5cf45f5ce0a11c921a2999bf2342df18b26
Nuntius Creative Hub <hola@nuntius.dev>
2024-12-15 16:13:48 -0500

    Update startubuntu.sh

--- startubuntu.sh
+++ startubuntu.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # Detener posibles procesos conflictivos
-killall termux-x11 2>/dev/null
+if pgrep termux-x11 > /dev/null; then
+    killall termux-x11
+fi
 
 # Iniciar el servidor X11
 termux-x11 :1 &
-am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
+am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity >/dev/null 2>&1
 
 # Esperar a que termux-x11 esté listo
-sleep 2
+while ! pgrep Xvnc > /dev/null; do
+    sleep 1
+done
 
 # Configurar variables de entorno
 export DISPLAY=:1
