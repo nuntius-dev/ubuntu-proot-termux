69c61de4972fc556e19359928bbb01a1b4fa1639
Nuntius Creative Hub <hola@nuntius.dev>
2024-12-17 20:57:05 +0000

    Actualizar startubuntu.sh

--- startubuntu.sh
+++ startubuntu.sh
@@ -6,17 +6,17 @@ if pgrep termux-x11 > /dev/null; then
 fi
 
 # Iniciar el servidor X11
-termux-x11 :1 &
-am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity >/dev/null 2>&1
+#termux-x11 :1 &
+#am start --user 0 -n com.termux.x11/#com.termux.x11.MainActivity >/dev/null #2>&1
 
 # Esperar a que termux-x11 esté listo
-while ! pgrep Xvnc > /dev/null; do
-    sleep 1
-done
+#while ! pgrep Xvnc > /dev/null; do
+#    sleep 1
+#done
 
 # Configurar variables de entorno
-export DISPLAY=:1
-export PULSE_SERVER=127.0.0.1
+#export DISPLAY=:1
+#export PULSE_SERVER=127.0.0.1
 
 # Iniciar Ubuntu con entorno gráfico
-GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 proot-distro login ubuntu --shared-tmp -- /bin/bash -c "startxfce4"
+proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1 startxfce4
